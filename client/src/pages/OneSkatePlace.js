d5bf3880e042bbcaf466d3f32efa5e6b082ce458
Jada Arnett <jada.arnett@gmail.com>
2020-11-04 14:24:39 -0500

    adding a pages folder with page files for each page

---
+++ client/src/pages/OneSkatePlace.js
@@ -0,0 +1,9 @@
+function OneSkatePlace () {
+    return (
+        <div>
+            <h1>This is one skate place page.</h1>
+        </div>
+    )
+}
+
+export default OneSkatePlace;
---
+++ client/src/pages/UserDashboard.js
@@ -0,0 +1,9 @@
+function UserDashboard () {
+    return (
+        <div>
+            <h1>This is the user dashboard page.</h1>
+        </div>
+    )
+}
+
+export default UserDashboard;
