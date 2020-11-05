2d435fb6951c436e987c56006c01021a14a3de5e
Jude Clark <judeclark2019@gmail.com>
2020-11-05 11:45:34 -0500

    no

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,14 @@
-function Signup () {
-    return (
-        <div>
-            <h1>This is the signup page.</h1>
-        </div>
-    )
+import Input from "../components/Input";
+import DatePicker from "../components/DatePicker";
+
+function Signup() {
+  return (
+    <div>
+      <h1>This is the signup page.</h1>
+      <Input label="testlabel" />
+      <DatePicker />
+    </div>
+  );
 }
 
 export default Signup;
