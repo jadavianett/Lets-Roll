0e0e6bf14c4d64d6249ecc8b2f706a9103edbf60
Phantom <fay.ashbro@gmail.com>
2020-11-05 10:31:15 -0500

    created the signup component and added to the sign up page

---
+++ client/src/components/Signup/SignupComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignupComponent = () => {
+    return (
+        <div>
+            <h1> pizza</h1>
+        </div>
+    );
+};
+
+export default SignupComponent;
