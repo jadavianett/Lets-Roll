0fa9de7a6ae87df7f4d67afac453252c9cdd310c
Jada Arnett <jada.arnett@gmail.com>
2020-11-05 10:59:49 -0500

    adding layout for all places page and importing card component

--- client/src/pages/AllPlaces.js
+++ client/src/pages/AllPlaces.js
@@ -1,7 +1,14 @@
+import Card from "../components/Places/Card";
 function AllPlaces () {
     return (
         <div>
             <h1>This is the all skate places page.</h1>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
     )
 }
