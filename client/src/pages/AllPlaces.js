f194eb12720e95c83adbfcd9c58d64d349b3ebfb
Jude Clark <judeclark2019@gmail.com>
2020-11-06 17:57:09 -0500

    set up props on the All places card in preparation for mapping

--- client/src/pages/AllPlaces.js
+++ client/src/pages/AllPlaces.js
@@ -1,16 +1,25 @@
 import AllPlacesCard from "../components/Places/AllPlacesCard";
-function AllPlaces () {
-    return (
-        <div>
-            <h1>This is the all skate places page.</h1>
-            <AllPlacesCard/>
-            <AllPlacesCard/>
-            <AllPlacesCard/>
-            <AllPlacesCard/>
-            <AllPlacesCard/>
-            <AllPlacesCard/>
-        </div>
-    )
+function AllPlaces() {
+  return (
+    <div>
+      <h1>This is the all skate places page.</h1>
+      <AllPlacesCard
+        image="https://via.placeholder.com/345x140.png"
+        placeName="Skate Place Name"
+        location="place location"
+      />
+      <AllPlacesCard
+        image="https://via.placeholder.com/345x140.png"
+        placeName="Skate Place Name"
+        location="place location"
+      />{" "}
+      <AllPlacesCard
+        image="https://via.placeholder.com/345x140.png"
+        placeName="Skate Place Name"
+        location="place location"
+      />
+    </div>
+  );
 }
 
 export default AllPlaces;
