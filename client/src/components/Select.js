818d0551589fe376372ef4ceb76cfb6e74d4a6b9
Phantom <fay.ashbro@gmail.com>
2020-11-08 21:17:56 -0500

    brought in Select component

---
+++ client/src/components/Select.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function NativeSelects(props) {
+  const classes = useStyles();
+
+
+  return (
+    <div>
+    
+  
+    
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel htmlFor="outlined-age-native-simple">Type</InputLabel>
+        <Select
+          native
+          value={props.type}
+          onChange={props.onChangeInfo}
+          label="Type"
+          inputProps={{
+            name: 'type',
+            id: 'outlined-age-native-simple',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={"Skate Park"}>Skate Park</option>
+          <option value={"Outdoor rink"}>Outdoor rink</option>
+          <option value={"Beltline"}>Beltline</option>
+          <option value={"Trial"}>Trail</option>
+          <option value={"Greenway"}>Greenway</option>
+          <option value={"Park"}>Park</option>
+          <option value={"Other"}>Other</option>
+          
+        </Select>
+      </FormControl>
+      
+    </div>
+  );
+}
