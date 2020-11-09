39d69ce88fb20c1dff0f0011f17ac087cc5bde57
Jude Clark <judeclark2019@gmail.com>
2020-11-09 11:50:00 -0500

    edit place page pre-populates with place data

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,57 +1,53 @@
 import axios from "axios";
 
-
 export default {
-    //USER CALLS 
-    // Get All Users 
-    getUser: function() {
-      return axios.get("/api/user");
-    },
-    //create a new User 
-    createUser: function (userData){
-        return axios.post("/api/user",userData)
-    },
+  //USER CALLS
+  // Get All Users
+  getUser: function () {
+    return axios.get("/api/user");
+  },
+  //create a new User
+  createUser: function (userData) {
+    return axios.post("/api/user", userData);
+  },
 
-    updateUser: function (id){
-        return axios.put("/api/user/" + id)
-    },
+  updateUser: function (id) {
+    return axios.put("/api/user/" + id);
+  },
 
-    //PLACES CALLS 
+  //PLACES CALLS
 
-    //Get All Places
-    getPlaces: function () {
-        return axios.get("/api/places")
-    }, 
+  //Get All Places
+  getPlaces: function () {
+    return axios.get("/api/places");
+  },
 
-    // Get Place by id 
-    getPlace: function (id) {
-        return axios.get("/api/places" + id)
-    }, 
+  // Get Place by id
+  getPlace: function (id) {
+    return axios.get("/api/places/" + id);
+  },
 
-    //Create a Place 
-    createPlace: function (placeData) {
-        return axios.post("/api/places", placeData)
-    }, 
+  //Create a Place
+  createPlace: function (placeData) {
+    return axios.post("/api/places", placeData);
+  },
 
-    //Update Place 
-    updatePlace: function (id) {
-        return axios.put("/api/places" +id)
-    }, 
+  //Update Place
+  updatePlace: function (id) {
+    return axios.put("/api/places" + id);
+  },
 
-    //Delete Place 
-    deletePlace: function (id) {
-        return axios.delete("/api/places" + id)
-    }
+  //Delete Place
+  deletePlace: function (id) {
+    return axios.delete("/api/places" + id);
+  },
 
-
-    // // Deletes the post with the given id
-    // deletePost: function(id) {
-    //   return axios.delete("/api/posts/" + id);
-    // },
-    // // Saves a post to the database
-    // savePost: function(postData) {
-    //   return axios.post("/api/posts", postData);
-    // }
-    
-  };
-  
+  // // Deletes the post with the given id
+  // deletePost: function(id) {
+  //   return axios.delete("/api/posts/" + id);
+  // },
+  // // Saves a post to the database
+  // savePost: function(postData) {
+  //   return axios.post("/api/posts", postData);
+  // }
+};
--- client/src/pages/EditPlace.js
+++ client/src/pages/EditPlace.js
@@ -1,13 +1,78 @@
-import AllPlacesCard from "../components/Places/AllPlacesCard";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import TextInput from "../components/TextInput";
+import Select from "../components/Select";
+import Button from "@material-ui/core/Button";
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import API from "../Utils/API";
 
 function EditPlace() {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [type, setType] = useState("");
+  //   const [notes, setNotes] = useState("");
+  const { id } = useParams();
+
+  useEffect(() => {
+    //code Here
+    API.getPlace(id)
+      .then((res) => {
+        console.log(res.data);
+        setName(res.data.name);
+        setLocation(res.data.location);
+        setType(res.data.type);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
+  const onChangeInfo = (e) => {
+    let value = e.target.value;
+
+    if (e.target.name === "name") {
+      setName(value);
+    } else if (e.target.name === "location") {
+      setLocation(value);
+      // } else if (e.target.name === "notes") {
+      //   setNotes(value);
+    } else if (e.target.name === "type") {
+      setType(value);
+      //   console.log("State changed: place type");
+    }
+  };
+
   return (
-    <>
-      <h1>This is the edit place page</h1>
-    </>
+    <div>
+      <h1>Edit Skate Place</h1>
+      <label for="name">Name</label>
+      <TextInput name="name" placeholder={name} onChange={onChangeInfo} />
+      <label for="location">Location</label>
+      <TextInput
+        name="location"
+        placeholder={location}
+        onChange={onChangeInfo}
+      />
+      {/* <TextInput
+        name="notes"
+        placeholder="NOTES"
+        onChange={onChangeInfo}
+      /> */}
+      <label for="type">Skate Place Type</label>
+      <Select name="type" onChange={onChangeInfo} value={type} />
+      <Button
+        type="submit"
+        size="large"
+        variant="contained"
+        onClick={handleSave}
+      >
+        Save Place
+      </Button>
+    </div>
   );
 }
 
