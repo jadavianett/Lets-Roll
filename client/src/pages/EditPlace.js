3baf09409d8359742c0f225a85df4fe270a21db9
Jude Clark <judeclark2019@gmail.com>
2020-11-09 11:33:22 -0500

    route for edit place page

---
+++ client/src/pages/EditPlace.js
@@ -0,0 +1,14 @@
+import AllPlacesCard from "../components/Places/AllPlacesCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import API from "../Utils/API";
+
+function EditPlace() {
+  return (
+    <>
+      <h1>This is the edit place page</h1>
+    </>
+  );
+}
+
+export default EditPlace;
