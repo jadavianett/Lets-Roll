0a7464c569fc2bc8ba7975f75dcba11cc490633d
Jude Clark <judeclark2019@gmail.com>
2020-11-10 18:57:30 -0500

    I forgot one! styled one skate place page

--- client/src/components/Paper.jsx
+++ client/src/components/Paper.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
       margin: theme.spacing(1),
       width: theme.spacing(16),
       height: theme.spacing(16),
@@ -19,8 +19,8 @@ export default function SimplePaper() {
 
   return (
     <div className={classes.root}>
-      <Paper>
-          Here is information.
+      <Paper className="wide-paper" elevation={5}>
+        Here is information.
       </Paper>
     </div>
   );
--- client/src/pages/OneSkatePlace.js
+++ client/src/pages/OneSkatePlace.js
@@ -1,20 +1,20 @@
+import "./OneSkatePlace.css";
 import Paper from "../components/Paper";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import API from "../Utils/API";
 
 function OneSkatePlace() {
   const { id } = useParams();
-  const [place, setPlace] = useState({})
-  
+  const [place, setPlace] = useState({});
+
   useEffect(() => {
-    
     API.getPlace(id)
       .then((res) => {
         console.log("one skate place" + res.data);
- setPlace(res.data);
+        setPlace(res.data);
       })
       .catch((err) => {
         throw err;
@@ -22,23 +22,34 @@ function OneSkatePlace() {
   }, [id]);
 
   return (
-    <Container>
-      <h1>This is one skate place page.</h1>
-      <h1>{place.name}</h1>
-      <img
-        src="https://via.placeholder.com/300x300.png"
-        alt="placeholder"
-      ></img>
-      <h3>See what others have said about this place!</h3>
-      <Paper />
-      <Paper />
-      <Paper />
-      <p>
-        <Link to="/allplaces">
-          <Button variant="contained">VIEW ALL SKATE PLACES</Button>
-        </Link>
-      </p>
-    </Container>
+    <>
+      <div className="page-wrapper-with-nav">
+        <div id="one-place-wrapper">
+          <h1>{place.name}</h1>
+          <br />
+          <br />
+          <img
+            src="https://via.placeholder.com/300x300.png"
+            alt="placeholder"
+          ></img>
+          <br />
+          <br /> <br /> <br />
+          <h3>See what others have said about this place!</h3>
+          <br />
+          <br />
+          <Paper />
+          <Paper />
+          <Paper />
+          <p>
+            <br />
+            <br />
+            <Link to="/allplaces">
+              <Button variant="contained">VIEW ALL SKATE PLACES</Button>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
