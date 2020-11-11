8d81c6978cd3a2dae2c3175afdbe2b4ed686772f
Phantom <fay.ashbro@gmail.com>
2020-11-11 12:07:22 -0500

    put in jwt information

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -79,7 +79,7 @@ function Signup() {
     })
       .then((res) => {
         console.log("signup" + res.data);
-        setJwt()
+        setJwt(res.data.data)
         history.push("/userdashboard");
       })
       .catch((err) => {
