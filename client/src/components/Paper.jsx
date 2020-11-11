49d4c7699d086f03fa38648d53e9c54d5344d81d
Jada Arnett <jada.arnett@gmail.com>
2020-11-11 12:02:50 -0500

    added props.notes to render to page

--- client/src/components/Paper.jsx
+++ client/src/components/Paper.jsx
@@ -8,19 +8,19 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
     "& > *": {
       margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
+      width: theme.spacing(100),
+      height: theme.spacing(10),
     },
   },
 }));
 
-export default function SimplePaper() {
+export default function SimplePaper(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Paper className="wide-paper" elevation={5}>
-        Here is information.
+        {props.notes}
       </Paper>
     </div>
   );
