694f84a62c1489dadd1aac3d0e85551b3ea1a1bc
Jude Clark <judeclark2019@gmail.com>
2020-11-11 16:50:37 -0500

    simple logout page

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React from "react";
+import "./Logout.css";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
-import {useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 
 const Logout = () => {
+  const { jwt, setJwt } = useContext(AuthContext);
 
-    const { jwt, setJwt } = useContext(AuthContext);
+  useEffect(() => {
+    const sessionJwt = sessionStorage.getItem("jwt");
+    if (sessionJwt) {
+      sessionStorage.setItem("jwt", "");
+      setJwt("");
+    }
+  }, []);
 
-    useEffect(()=>{
-        const sessionJwt = sessionStorage.getItem("jwt");
-        if(sessionJwt)
-        {
-            sessionStorage.setItem("jwt", "");
-            setJwt("");
-          
-        }
-        
-    
-      },[]);
-    
-    return (
-        <div>
-            <h4>Successfully logout!!</h4>
+  return (
+    <>
+      <div className="page-wrapper-with-nav">
+        <div id="logout-wrapper">
+          <h1>You have successfully logged out.</h1>
+          <br />
+          <p>You may still:</p>
+          <br />
+          <div id="dash-links">
+            <p class="btm-margin">
+              <Link to="/allplaces">
+                <Button variant="contained" className="full-width">
+                  VIEW ALL SKATE PLACES
+                </Button>
+              </Link>
+            </p>
+
+            <p class="btm-margin">
+              <Link to="/tutorials">
+                <Button variant="contained" className="full-width">
+                  VIEW TUTORIALS
+                </Button>
+              </Link>
+            </p>
+
+            <p>
+              <Link to="/login">
+                <Button variant="contained" className="full-width">
+                  LOG BACK IN
+                </Button>
+              </Link>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Logout;
