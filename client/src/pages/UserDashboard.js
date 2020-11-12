db5aec4c10ff3c777749526bafb4326c841ddf39
Jada Arnett <jada.arnett@gmail.com>
2020-11-12 10:31:40 -0500

    adding icons to dash

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -4,6 +4,11 @@ import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import React, { useContext, useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import AddTwoToneIcon from '@material-ui/icons/AddTwoTone';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExploreIcon from '@material-ui/icons/Explore';
+import ListIcon from '@material-ui/icons/List';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 function UserDashboard() {
   const [user, setUser] = useState({});
@@ -31,35 +36,35 @@ function UserDashboard() {
             <p class="btm-margin">
               <Link to="/allplaces">
                 <Button variant="contained" className="full-width">
-                  VIEW ALL SKATE PLACES
+                  VIEW ALL SKATE PLACES <ExploreIcon/>
                 </Button>
               </Link>
             </p>
             <p class="btm-margin">
               <Link to="/tutorials">
                 <Button variant="contained" className="full-width">
-                  VIEW TUTORIALS
+                  VIEW TUTORIALS <VideoLibraryIcon/>
                 </Button>
               </Link>
             </p>
             <p class="btm-margin">
               <Link to="/addnewplace">
                 <Button variant="contained" className="full-width">
-                  ADD NEW SKATE PLACE
+                  ADD NEW SKATE PLACE <AddTwoToneIcon/>
                 </Button>
               </Link>
             </p>{" "}
             <p class="btm-margin">
               <Link to="/viewmyplaces">
                 <Button variant="contained" className="full-width">
-                  VIEW MY SKATE PLACES
+                  VIEW MY SKATE PLACES <ListIcon />
                 </Button>
               </Link>
             </p>
             <p>
               <Link to="/logout">
                 <Button variant="contained" className="full-width">
-                  LOG OUT
+                  LOG OUT <ExitToAppIcon/>
                 </Button>
               </Link>
             </p>
